8266ef012e022cfe3ba94a676feda8aa4f7e46a3
Grif_on <91139654+grif-on@users.noreply.github.com>
2024-05-30 17:15:42 +0300

    Added code snipet for opening simple console

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 #include "Window.h"
 
 int CALLBACK WinMain(HINSTANCE app_instance, HINSTANCE previous_instance, LPSTR arguments, int arguments_count) {
+  
+  //AllocConsole();
+  //// requires #include <iostream> at the top of the file
+  //freopen("conin$", "r", stdin);
+  //freopen("conout$", "w", stdout);
+  //freopen("conout$", "w", stderr);
 
   Window window(100, 100, 800, 600);
 
